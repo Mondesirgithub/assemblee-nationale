6cebf376d2eb9b820d59078f4c7c66e29d0dbc2e
Mondesir <ridelntsoumou@gmail.com>
2023-05-15 20:40:12 +0200

    creation des templates des articles de cat, les details de l'article

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'comptes'
+app_name = 'web'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('articles/', views.articles, name='articles'),
+    path('article-details/', views.article_details, name='article-details'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Categorie
+from .models import Categorie, Article
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -9,3 +10,35 @@ def index(request):
     }
     
     return render(request, 'web/index.html', context)
+
+
+
+def article_details(request, catId):
+    context = {}
+    try:
+        article = Article.objects.get(pk=catId)
+        categorie = Categorie.objects.get(pk=article.pk)
+        articles = categorie.article_set.all()
+        context = {
+            'article': article,
+            'articles': articles
+        }
+    except:
+        messages.error(request, "L'article n'existe pas")
+
+    return render(request, 'web/article_details.html', context)
+
+
+def articles(request, pk):
+    context = {'article':None}
+    try:
+        categorie = Categorie.objects.get(pk=pk)
+        articles = categorie.articles_set.all()
+        context = {
+            'articles': articles,
+            'categorie': categorie
+        }
+    except:
+        messages.error(request, "La categorie n'existe pas")
+
+    return render(request, 'web/articles.html', context)
