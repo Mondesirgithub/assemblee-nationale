c12a359350b024153a36791945f1d8feb73d8ebe
Mondesir <ridelntsoumou@gmail.com>
2023-05-16 19:13:08 +0200

    preparing to copy forum from internet

--- forums/admin.py
+++ forums/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import CategorieSujet, Sujet, Post
 # Register your models here.
+
+
+admin.site.register(CategorieSujet)
+admin.site.register(Sujet)
+admin.site.register(Post)
--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from comptes.models import Depute
 
-# Create your models here.
+
+class CategorieSujet(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Sujet(models.Model):
+    contenu = models.CharField(max_length=200)
+    categorie = models.ForeignKey(CategorieSujet, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(Depute, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.contenu
+
+
+class Post(models.Model):
+    contenu = models.TextField()
+    sujet = models.ForeignKey(Sujet, on_delete=models.CASCADE)
+    parent_post = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    created_by = models.ForeignKey(Depute, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.pk} par {self.created_by.username}"
