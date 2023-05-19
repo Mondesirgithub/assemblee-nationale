fe7edb233ab322a6c3e6b5b9f274c83025f70a33
UrbanoJefra <urbanojefra688@gmail.com>
2023-05-19 20:54:00 +0100

    travail terminer

--- assemblee_nationale/settings.py
+++ assemblee_nationale/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-68xzqii+aarcc0a4fxil(5j$va&r513ei@j$$&i4u#vj6gfp1n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
